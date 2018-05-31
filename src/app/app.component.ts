4dc896097949966e869a842d2218eba011d4d019
slimaneguemil <sguemil>
2018-05-31 13:14:54 +0200

    end course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+ import { Component } from '@angular/core';
+import {AuthService} from "./services/auth.service";
 
-@Component({
+ @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'jsjobs';
+  constructor (private authService:AuthService){};
 }
